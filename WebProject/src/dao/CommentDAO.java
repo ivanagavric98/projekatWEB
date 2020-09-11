ba30efe6200e0340ddd2bea985ff4b1877f2c352
ivana.gavric <gavric.ra160.2017@uns.ac.rs>
2020-09-11 18:23:43 +0200

    #dodavanje komentara, back

--- WebProject/src/dao/CommentDAO.java
+++ WebProject/src/dao/CommentDAO.java
@@ -1,5 +1,66 @@
 package dao;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collection;
+import java.util.HashMap;
+
+import javax.servlet.http.Cookie;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import model.Comment;;
+
 public class CommentDAO {
 
+	private HashMap<Long,Comment> comments = new HashMap<Long, Comment>();
+	private String contextPath;
+	
+	
+	public CommentDAO(String contextPath) throws NoSuchAlgorithmException, IOException {
+		this.contextPath = contextPath;
+		comments = loadComments(contextPath);
+	}
+
+	public Collection<Comment> findAllComments() {
+		return comments.values();
+	}
+	
+	public Comment addNewComment(Comment comment) throws NoSuchAlgorithmException, IOException {
+		
+		long id = comments.size() + 1;
+		
+		Comment com = new Comment(
+					id,
+					comment.getGuest(),
+					comment.getApartment(),
+					comment.getText(),
+					comment.getRatingOfApartments());
+		
+		comments.put(id, com);
+		saveComments(contextPath);
+		return comment;
+	}
+	
+	
+	
+	public HashMap<Long,Comment> loadComments(String contextPath) throws IOException, NoSuchAlgorithmException {
+	    ObjectMapper mapper = new ObjectMapper();
+	    File commentFile = new File(contextPath + "/comments.json");
+	    boolean success = commentFile.createNewFile();
+	    if (success) {
+	       mapper.writeValue(commentFile, comments);
+	    }
+	    return mapper.readValue(commentFile, new TypeReference<HashMap<Long,Comment>>() {});
+	}
+
+	public void saveComments(String contextPath) throws IOException, NoSuchAlgorithmException {
+	    ObjectMapper mapper = new ObjectMapper();
+	    File commentFile = new File(contextPath + "/comments.json");
+	    commentFile.createNewFile();
+	    mapper.writeValue(commentFile, comments);
+		}
+	
 }
--- WebProject/src/services/CommentService.java
+++ WebProject/src/services/CommentService.java
@@ -1,5 +1,78 @@
 package services;
 
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import dao.ApartmentDAO;
+import dao.CommentDAO;
+import dao.UserDAO;
+import model.Apartment;
+import model.Comment;
+import model.User;
+
+@Path("comment")
 public class CommentService {
 
+	@Context
+	ServletContext ctx;
+	
+	public CommentService() {
+		
+	}
+	
+	@PostConstruct
+	public void init() throws NoSuchAlgorithmException, IOException {
+	
+		if (ctx.getAttribute("commentDAO") == null) {
+	    	String contextPath = ctx.getRealPath("");
+			ctx.setAttribute("commentDAO", new CommentDAO(contextPath));
+		}
+		
+	}
+	
+	@GET
+	@Path("/findAllComments")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Collection<Comment> findAll(@Context HttpServletRequest request){
+		CommentDAO commentsDAO = (CommentDAO) ctx.getAttribute("commentDAO");
+		return commentsDAO.findAllComments();		
+	
+	}
+	
+	@POST
+	@Path("/addComment")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response comments(Comment comment, @Context HttpServletRequest request) throws NoSuchAlgorithmException, IOException {
+		CommentDAO commentsDAO = (CommentDAO) ctx.getAttribute("commentDAO");
+		System.out.println("successfully added");
+		
+		User guest = (User) request.getSession().getAttribute("user");
+		String guest_username = guest.getUsername();
+		
+		Comment successfulAdd = commentsDAO.addNewComment(comment);		
+		if(successfulAdd != null) {
+			return Response.status(200).build();	
+		}else {
+			return Response.status(400).entity("Apartment is already exist.").build();
+		}
+		
+	}
+	
+	
+	
+	
 }
