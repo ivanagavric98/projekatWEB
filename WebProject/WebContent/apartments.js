19e05e35929b11b805f2f9bfb2abcad118b933fe
ivana.gavric <gavric.ra160.2017@uns.ac.rs>
2020-09-14 15:41:39 +0200

    #kreiranje rezervacija modal, front

--- WebProject/WebContent/apartments.js
+++ WebProject/WebContent/apartments.js
@@ -9,18 +9,14 @@
      let checkInTime = $('<td>' + apartment.checkInTime + '</td>');
      let checkOutTime = $('<td>' + apartment.checkOutTime + '</td>');
      let status = $('<td>' + apartment.status + '</td>');
-     let createRes = $('<td>'  + '<input type="submit" name="Create" id="createReservation_button">' + '</td>');
-	 
+     let createRes = $('<td>' + '<button type="submit" id="createReservation_button">Create</button>' + '</td>');
+
 
      let tr = $('<tr></tr>');
      tr.append(id).append(type).append(roomsNumber).append(guestsNumber)
          .append(location).append(host).append(pricePerNight).append(checkInTime).append(checkOutTime).append(status).append(createRes);
      $('#admin-list-apartments-table').append(tr);
  }
-
-  $('#createReservation_button').click(function() {
-        $('#mod_pretraga').show();
-    });
 
  $(document).ready(function() {
      $('#apartments_li').click(function() {
@@ -39,38 +35,44 @@
          })
      });
 
-     $('#add_apartment_submit_form').click(function(event){
+     $('#createReservation_button').on('click', function() {
+         $('#mod_pretraga').show();
+     });
 
-        console.log('addd');
-		let type = $("#addApartmants input[type='radio']:checked").val();
 
+     $('#add_apartment_submit_form').click(function(event) {
 
-        let dataObj = {
-            "roomsNumber" : $("input[name=numberOfRooms]").val(),
-            "guestsNumber" : $("input[name=numberOfGuest]").val(),
-            "location" : $("input[name=Location]").val(),
-            "datumi" : $("input[name=datumi]").val(),
-            "comments" : $("input[name=comments]").val(),
-            "pricePerNight" : $("input[name=pricePerNight]").val(),
-            "checkInTime" : $("input[name=checkInTime]").val(),
-            "checkOutTime" : $("input[name=checkOutTime]").val(),
-            "sadrzajApartmana" : $("input[name=sadrzajApartmana]").val(),
-            "type" : type
-        };
+         console.log('addd');
+         let type = $("#addApartmants input[type='radio']:checked").val();
 
-        $.ajax({
-            type: 'POST',
-            url: 'rest/apartment/addAp',
-            data: JSON.stringify(dataObj),
-            contentType : 'application/json',
-            success : function(response){
-                console.log(response);
-                alert("Uspesno dodavanje!");
-                
-            }
-        });	
-		
-		event.preventDefault();
+         let date = ("#datumi");
+
+         let dataObj = {
+             "roomsNumber": $("input[name=numberOfRooms]").val(),
+             "guestsNumber": $("input[name=numberOfGuest]").val(),
+             "location": $("input[name=Location]").val(),
+             "datumi": $("input[name=datumi]").val(),
+             "comments": $("input[name=comments]").val(),
+             "pricePerNight": $("input[name=pricePerNight]").val(),
+             "checkInTime": $("input[name=checkInTime]").val(),
+             "checkOutTime": $("input[name=checkOutTime]").val(),
+             "sadrzajApartmana": $("input[name=sadrzajApartmana]").val(),
+             "type": type
+         };
+
+         $.ajax({
+             type: 'POST',
+             url: 'rest/apartment/addAp',
+             data: JSON.stringify(dataObj),
+             contentType: 'application/json',
+             success: function(response) {
+                 console.log(response);
+                 alert("Uspesno dodavanje!");
+
+             }
+         });
+
+         event.preventDefault();
      });
 
 
